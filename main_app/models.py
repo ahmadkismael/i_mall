c07191907711a16309928ae2d853d044aa86c26b
ahmadkismael <ahmad.abualkhair@gmail.com>
2023-04-03 13:13:35 -0600

    worked on shippingaddress model

--- main_app/models.py
+++ main_app/models.py
@@ -36,4 +36,17 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=0, blank=True, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    zipcode = models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
     
