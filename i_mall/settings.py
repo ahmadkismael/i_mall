eabe851bf7f8c6573e349f748dc401b8f71424d2
Piquant95 <piquant95@yahoo.com>
2023-04-04 09:52:45 -0400

    fixed icons

--- i_mall/settings.py
+++ i_mall/settings.py
@@ -12,19 +12,19 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
-import environ
+# import environ
 
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-environ.Env()
-environ.Env.read_env()
+# environ.Env()
+# environ.Env.read_env()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
